ac2669723ede6a4cffb3f784fdcfb7b843d35399
aliendro <mgbaliendro@gmail.com>
2022-01-17 18:38:27 -0300

    Fix: change footer styling

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,19 +2,13 @@ const Footer = () => {
   const styles = {
     footer:
       'flex flex-col gap-2 items-center justify-center font-mono text-sm  text-foreground transition-colors duration-300 h-20 cursor-default mt-40',
-    copywright: 'text-[10px]',
-    license: 'hover:text-yellow hover:cursor-pointer',
   };
 
   return (
     <footer className={styles.footer}>
-      <h1>Built by Michael Aliendro</h1>
-      <h2 className={styles.copywright}>
-        Dracula theme made by Zeno Rocha under{' '}
-        <a className={styles.license} href="https://zenorocha.mit-license.org/">
-          MIT license.
-        </a>
-      </h2>
+      <h1>
+        Made with <span className="text-2xl text-purple">&hearts;</span> by Michael Aliendro
+      </h1>
     </footer>
   );
 };
