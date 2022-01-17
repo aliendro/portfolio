6c9eb2e9661d9820f125625a93ba0a9f206f6b69
aliendro <mgbaliendro@gmail.com>
2022-01-17 18:39:36 -0300

    Add: responsive layout for mobile devices

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from 'react';
 
-import { Menu, Close } from 'assets';
+import { Menu, Close, Linkedin, Github, Email } from 'assets';
 import useScroll from 'hooks/useScroll';
 
 const styles = {
-  header: 'font-mono text-sm text-foreground fixed w-full z-10 bg-primary transition-all',
+  header:
+    'bg-secondary font-mono text-sm text-foreground fixed w-full z-10 bg-primary transition-all duration-500',
   nav: 'm-5 mx-14 flex items-center',
   navbar:
-    'flex flex-col w-64 h-screen absolute transition-transform duration-500 md:duration-50 top-0 bg-secondary  right-0 md:w-auto md:h-auto md:static md:flex-row gap-5 items-center justify-center h-20',
+    'flex flex-col w-64 h-screen absolute transition-transform duration-500 md:duration-50 top-0 bg-secondary right-0 md:w-auto md:h-auto md:static md:flex-row gap-5 items-center justify-center h-20',
   logo: 'p-2 w-8 h-8 border-green border-2 mr-auto ring-purple ring-2 ring-offset-8 ring-offset-secondary rounded-t-2xl rounded-br-2xl font-sans flex items-center font-extrabold hover:ring-green hover:border-purple transition-colors duration-200',
   navitem:
-    'p-5 hover:underline hover:cursor-pointer decoration-white decoration-2 underline-offset-4 transition-all duration-150 hover:text-green',
+    'p-5 hover:underline hover:cursor-pointer decoration-white decoration-2 underline-offset-4  hover:text-green',
   button:
     'p-3 border-white border-[1.5px] rounded hover:bg-white transition-colors duration-200 hover:text-white',
-  mobile: 'md:hidden',
   icon: 'text-green h-10 w-10 z-10 md:hidden',
 };
 
@@ -24,45 +24,59 @@ const links = {
 };
 
 const Navbar = () => {
-  const [menu, setMenu] = useState<boolean>(false);
-  const scrollVisible = useScroll();
-  const isMenuOpen = menu
-    ? `${styles.navbar} translate-x-0`
-    : `${styles.navbar} translate-x-72 md:translate-x-0 `;
-  const header = scrollVisible
-    ? `${styles.header} visible bg-secondary`
-    : `${styles.header} invisible`;
+  const [navbarOpen, setNavbarOpen] = useState<boolean>(false);
+  const headerVisible = useScroll();
+  const navbarClass = navbarOpen
+    ? `${styles.navbar} transform-none`
+    : `${styles.navbar} translate-x-72 md:transform-none `;
+  const headerClass = headerVisible ? `${styles.header} top-0` : `${styles.header} -top-40`;
 
-  const openMenu = () => setMenu(true);
-  const closeMenu = () => setMenu(false);
+  const open = () => setNavbarOpen(true);
+  const close = () => setNavbarOpen(false);
 
   useEffect(() => {
-    if (menu) {
+    if (navbarOpen) {
       document.body.style.overflow = 'hidden';
     } else {
       document.body.style.overflow = 'unset';
     }
-  }, [menu]);
+  }, [navbarOpen]);
 
   return (
-    <header className={header}>
+    <header className={headerClass}>
       <nav className={styles.nav}>
-        <div className={styles.logo} />
-        <Menu className={`${styles.icon} ${menu ? 'hidden' : 'block'}`} onClick={openMenu} />
-        <Close className={`${styles.icon} ${menu ? 'block' : 'hidden'}`} onClick={closeMenu} />
-        <ul className={isMenuOpen}>
-          <li className={styles.navitem}>About</li>
-          <li className={styles.navitem}>Projects</li>
-          <li className={styles.navitem}>Resume</li>
-          <li className={`${styles.navitem} ${styles.mobile}`}>
-            <a href={links.linkedin}> LinkedIn</a>
+        <a className={styles.logo} href="/">
+          <div />
+        </a>
+        <Menu className={`${styles.icon} ${navbarOpen ? 'hidden' : 'block'}`} onClick={open} />
+        <Close className={`${styles.icon} ${navbarOpen ? 'block' : 'hidden'}`} onClick={close} />
+        <ul className={navbarClass}>
+          <li className={styles.navitem}>
+            <a href="#about">About</a>
           </li>
-          <li className={`${styles.navitem} ${styles.mobile}`}>
-            <a href={links.github}>Github</a>
+          <li className={styles.navitem}>
+            <a href="#projects">Projects</a>
           </li>
-          <li className={`${styles.navitem} ${styles.mobile}`}>
-            <a href={links.email}>Contact me</a>
+          <li className={styles.navitem}>
+            <a href="#resume">Resume</a>
           </li>
+          <ul className="flex fixed bottom-0 md:hidden">
+            <li className={styles.navitem}>
+              <a href={links.linkedin} target="_blank" rel="noreferrer">
+                <Linkedin />
+              </a>
+            </li>
+            <li className={styles.navitem}>
+              <a href={links.github} target="_blank" rel="noreferrer">
+                <Github />
+              </a>
+            </li>
+            <li className={styles.navitem}>
+              <a href={links.email} target="_blank" rel="noreferrer">
+                <Email />
+              </a>
+            </li>
+          </ul>
         </ul>
       </nav>
     </header>
