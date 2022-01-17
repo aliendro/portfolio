97a0f0e01227e549a0683b2c8305f0cd8eaac2a9
aliendro <mgbaliendro@gmail.com>
2022-01-17 18:37:17 -0300

    Add sidebar animation

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,8 +1,9 @@
 import { Linkedin, Github, Email } from 'assets';
+import { useEffect, useState } from 'react';
 
 const styles = {
   sidebar:
-    'hidden md:flex flex-col gap-10 fixed top-1/3 bg-secondary p-2 pt-8 pb-8 rounded shadow-sm shadow-secondary text-foreground ml-5',
+    'hidden md:flex flex-col gap-10 fixed bg-secondary p-2 pt-8 pb-8 rounded shadow-sm shadow-secondary text-foreground ml-5 transition-all duration-500',
   icon: 'h-7 w-7 hover:text-purple transition-colors duration-200 hover:cursor-pointer',
 };
 
@@ -12,24 +13,33 @@ const links = {
   email: 'mailto:contato@aliendro.com',
 };
 
-const Sidebar = () => (
-  <ul className={styles.sidebar}>
-    <li>
-      <a href={links.linkedin}>
-        <Linkedin className={styles.icon} />
-      </a>
-    </li>
-    <li>
-      <a href={links.github}>
-        <Github className={styles.icon} />
-      </a>
-    </li>
-    <li>
-      <a href={links.email}>
-        <Email className={styles.icon} />
-      </a>
-    </li>
-  </ul>
-);
+const Sidebar = () => {
+  const [sidebarClass, setSidebarClass] = useState<string>(`${styles.sidebar} -bottom-80`);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSidebarClass(`${styles.sidebar} bottom-1/2`);
+    }, 200);
+  }, []);
+  return (
+    <ul className={sidebarClass}>
+      <li>
+        <a href={links.linkedin} target="_blank" rel="noreferrer">
+          <Linkedin className={styles.icon} />
+        </a>
+      </li>
+      <li>
+        <a href={links.github} target="_blank" rel="noreferrer">
+          <Github className={styles.icon} />
+        </a>
+      </li>
+      <li>
+        <a href={links.email} target="_blank" rel="noreferrer">
+          <Email className={styles.icon} />
+        </a>
+      </li>
+    </ul>
+  );
+};
 
 export default Sidebar;
