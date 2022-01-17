ba903a51d15b8d6a5bddd320d956e15a99588997
aliendro <mgbaliendro@gmail.com>
2022-01-17 15:01:28 -0300

    Add tech stack icon assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,36 @@
-import { ReactComponent as Linkedin } from 'assets/linkedin.svg';
-import { ReactComponent as Github } from 'assets/github.svg';
-import { ReactComponent as Email } from 'assets/email.svg';
+// Actions
 import { ReactComponent as Menu } from 'assets/menu.svg';
 import { ReactComponent as Close } from 'assets/close.svg';
 
-export { Linkedin, Github, Email, Menu, Close };
+// Companies
+import { ReactComponent as Linkedin } from 'assets/linkedin.svg';
+import { ReactComponent as Github } from 'assets/github.svg';
+import { ReactComponent as Email } from 'assets/email.svg';
+
+// Tech stack
+import { ReactComponent as Css } from 'assets/stack/css.svg';
+import { ReactComponent as Html } from 'assets/stack/html.svg';
+import { ReactComponent as Javascript } from 'assets/stack/javascript.svg';
+import { ReactComponent as React } from 'assets/stack/react.svg';
+import { ReactComponent as Redux } from 'assets/stack/redux.svg';
+import { ReactComponent as Tailwind } from 'assets/stack/tailwind.svg';
+import { ReactComponent as Typescript } from 'assets/stack/typescript.svg';
+import { ReactComponent as Jest } from 'assets/stack/jest.svg';
+import { ReactComponent as Nextjs } from 'assets/stack/nextjs.svg';
+
+export {
+  Linkedin,
+  Github,
+  Email,
+  Menu,
+  Close,
+  Css,
+  Html,
+  Javascript,
+  React,
+  Redux,
+  Tailwind,
+  Typescript,
+  Jest,
+  Nextjs,
+};
