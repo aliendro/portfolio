38aca3bd81db39a7a8fb74ebaf0fd56356543c2c
aliendro <mgbaliendro@gmail.com>
2022-01-21 18:57:45 -0300

    Add: tech stack and stylings

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,24 +1,82 @@
+import {
+  Css,
+  Git,
+  Html,
+  Javascript,
+  Jest,
+  Nextjs,
+  React,
+  Redux,
+  Router,
+  Tailwind,
+  TestingLibrary,
+  Typescript,
+} from 'assets';
+
 const styles = {
   section: 'grid md:grid-cols-[1fr_2fr_1fr] grid-flow-row gap-5 text-foreground',
-  heading: 'text-2xl md:col-start-2 font-bold tracking-wide pl-4 text-yellow',
+  heading: 'text-3xl md:col-start-2 font-bold tracking-wide pl-4 text-yellow text-center p-10',
   text: 'md:col-start-2 text-justify p-4 text-lg',
+  icon: 'w-14 h-14 text-green',
 };
 
-const About = () => (
-  <section className={styles.section}>
-    <h1 className={styles.heading}>About me</h1>
-    <h2 className={styles.text}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nisi eaque? Neque ipsam vitae
-      dicta alias provident architecto debitis quisquam quasi illum! Voluptatum vitae eveniet
-      consequatur? Nam dolores voluptatum, eum nulla consequuntur obcaecati quae reiciendis
-      repellendus illo ex amet dolorem numquam earum asperiores perspiciatis consectetur hic
-      exercitationem, beatae commodi molestias perferendis quod maiores veniam eligendi! Dolores
-      vitae nihil obcaecati expedita ut sit. Illo itaque ex dolores accusamus maiores a commodi sint
-      esse, voluptas natus dicta aliquam voluptatum molestiae dolorem odit saepe suscipit repellat!
-      Earum culpa deserunt possimus illo cumque iste, ea mollitia repellat at perspiciatis, quia
-      soluta sed non fugit?
-    </h2>
-  </section>
-);
+export default function About() {
+  return (
+    <section className={styles.section}>
+      <h1 id="stack" className={styles.heading}>
+        Tech stack
+      </h1>
+      <div className="md:col-start-2 grid grid-cols-2 md:grid-cols-3 place-items-center text-center gap-5 md:gap-10">
+        <div>
+          <Javascript className="w-20 h-20 m-5 text-yellow" />
+          <span className="text-md font-bold tracking-wide">Javascript</span>
+        </div>
+        <div>
+          <Html className="w-20 h-20 m-5 text-red" />
+          <span className="text-md font-bold tracking-wide">HTML</span>
+        </div>
+        <div>
+          <Css className="w-20 h-20 m-5 text-orange" />
+          <span className="text-md font-bold tracking-wide">CSS</span>
+        </div>
+        <div>
+          <React className="w-20 h-20 m-5 text-cyan" />
+          <span className="text-md font-bold tracking-wide">React</span>
+        </div>
+        <div>
+          <Redux className="w-20 h-20 m-5 text-purple" />
+          <span className="text-md font-bold tracking-wide">Redux</span>
+        </div>
+        <div>
+          <Tailwind className="w-20 h-20 m-5 text-cyan" />
+          <span className="text-md font-bold tracking-wide">TailwindCSS</span>
+        </div>
 
-export default About;
+        <div>
+          <TestingLibrary className="w-20 h-20 m-5 text-red" />
+          <span className="text-md font-bold tracking-wide ">Testing Library</span>
+        </div>
+        <div>
+          <Jest className="w-20 h-20 m-5 text-[#9A405B] " />
+          <span className="text-md font-bold tracking-wide">Jest</span>
+        </div>
+        <div>
+          <Git className="w-20 h-20 m-5 text-[#E94E31]" />
+          <span className="text-md font-bold tracking-wide ">Git</span>
+        </div>
+        <div>
+          <Typescript className="w-20 h-20 m-5 text-cyan" />
+          <span className="text-md font-bold tracking-wide">Typescript</span>
+        </div>
+        <div>
+          <Nextjs className="w-20 h-20 m-5 text-foreground" />
+          <span className="text-md font-bold tracking-wide">NextJS</span>
+        </div>
+        <div>
+          <Router className="w-20 h-20 m-5 text-red" />
+          <span className="text-md font-bold tracking-wide">React Router</span>
+        </div>
+      </div>
+    </section>
+  );
+}
