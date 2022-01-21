205eca481ae2b114b662e53a35369c6eae0b8e07
aliendro <mgbaliendro@gmail.com>
2022-01-21 18:57:57 -0300

    Fix: responsive layout

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -4,7 +4,7 @@ import Projects from 'components/projects/Projects';
 import GoTop from './GoTop';
 
 const styles = {
-  section: 'flex flex-col gap-40 mt-32 md:mt-40 justify-center w-full',
+  section: 'flex flex-col gap-12 md:gap-40 mt-32 md:mt-80 justify-center w-full',
 };
 
 export default function Content() {
