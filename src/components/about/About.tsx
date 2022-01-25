f15dc1a5afaa076a8649b10350394e7dcea3f8ac
aliendro <mgbaliendro@gmail.com>
2022-01-25 18:01:30 -0300

    Add: apply directives to reuse styling throughout application

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -13,68 +13,62 @@ import {
   Typescript,
 } from 'assets';
 
-const styles = {
-  section: 'grid md:grid-cols-[1fr_2fr_1fr] grid-flow-row gap-5 text-foreground',
-  heading: 'text-3xl md:col-start-2 font-bold tracking-wide text-yellow text-center p-10',
-  text: 'md:col-start-2 text-justify p-4 text-lg',
-  icon: 'w-14 h-14 text-green',
-};
-
 export default function About() {
+  const text = 'text-md font-bold tracking-wide';
   return (
-    <section className={styles.section}>
-      <h1 id="stack" className={styles.heading}>
+    <section className="section">
+      <h1 id="stack" className="heading text-yellow">
         Tech stack
       </h1>
       <div className="grid grid-cols-2 place-items-center gap-5 text-center md:col-start-2 md:grid-cols-3 md:gap-10">
         <div>
-          <Javascript className="m-5 h-20 w-20 text-yellow" />
-          <span className="text-md font-bold tracking-wide">Javascript</span>
+          <Javascript className="icon text-yellow" />
+          <span className={text}>Javascript</span>
         </div>
         <div>
-          <Html className="m-5 h-20 w-20 text-red" />
-          <span className="text-md font-bold tracking-wide">HTML</span>
+          <Html className="icon text-red" />
+          <span className={text}>HTML</span>
         </div>
         <div>
-          <Css className="m-5 h-20 w-20 text-orange" />
-          <span className="text-md font-bold tracking-wide">CSS</span>
+          <Css className="icon text-orange" />
+          <span className={text}>CSS</span>
         </div>
         <div>
-          <React className="m-5 h-20 w-20 text-cyan" />
-          <span className="text-md font-bold tracking-wide">React</span>
+          <React className="icon text-cyan" />
+          <span className={text}>React</span>
         </div>
         <div>
-          <Redux className="m-5 h-20 w-20 text-purple" />
-          <span className="text-md font-bold tracking-wide">Redux</span>
+          <Redux className="icon text-purple" />
+          <span className={text}>Redux</span>
         </div>
         <div>
-          <Tailwind className="m-5 h-20 w-20 text-cyan" />
-          <span className="text-md font-bold tracking-wide">TailwindCSS</span>
+          <Tailwind className="icon text-cyan" />
+          <span className={text}>TailwindCSS</span>
         </div>
 
         <div>
-          <TestingLibrary className="m-5 h-20 w-20 text-red" />
-          <span className="text-md font-bold tracking-wide ">Testing Library</span>
+          <TestingLibrary className="icon text-red" />
+          <span className={text}>Testing Library</span>
         </div>
         <div>
-          <Jest className="m-5 h-20 w-20 text-[#9A405B] " />
-          <span className="text-md font-bold tracking-wide">Jest</span>
+          <Jest className="icon text-[#9A405B] " />
+          <span className={text}>Jest</span>
         </div>
         <div>
-          <Git className="m-5 h-20 w-20 text-[#E94E31]" />
-          <span className="text-md font-bold tracking-wide ">Git</span>
+          <Git className="icon text-[#E94E31]" />
+          <span className={text}>Git</span>
         </div>
         <div>
-          <Typescript className="m-5 h-20 w-20 text-cyan" />
-          <span className="text-md font-bold tracking-wide">Typescript</span>
+          <Typescript className="icon text-cyan" />
+          <span className={text}>Typescript</span>
         </div>
         <div>
-          <Nextjs className="m-5 h-20 w-20 text-foreground" />
-          <span className="text-md font-bold tracking-wide">NextJS</span>
+          <Nextjs className="icon text-foreground" />
+          <span className={text}>NextJS</span>
         </div>
         <div>
-          <Router className="m-5 h-20 w-20 text-red" />
-          <span className="text-md font-bold tracking-wide">React Router</span>
+          <Router className="icon text-red" />
+          <span className={text}>React Router</span>
         </div>
       </div>
     </section>
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,19 +1,13 @@
-import Button from 'components/Button';
-
-const styles = {
-  section: 'grid md:grid-cols-[1fr_2fr_1fr] gap-5 text-foreground h-100 cursor-default',
-  heading: 'pl-4 font-mono text-green md:col-start-2',
-  bigHeading: 'pl-4 text-5xl md:text-6xl font-bold tracking-wider md:col-start-2',
-};
-
 export default function Hero() {
   return (
-    <section className={styles.section}>
-      <h1 className={styles.heading}>
+    <section className="section">
+      <h1 className="pl-4 font-mono text-green md:col-start-2">
         &gt; Hi, my name is <span className="duration-50 animate-pulse">_</span>
       </h1>
-      <h2 className={`${styles.bigHeading} text-slate-100`}>Michael Aliendro.</h2>
-      <h3 className={`${styles.bigHeading} text-slate-200`}>
+      <h2 className="text-slate-100 pl-4 text-5xl font-bold tracking-wider md:col-start-2 md:text-6xl">
+        Michael Aliendro.
+      </h2>
+      <h3 className="text-slate-200 pl-4 text-5xl font-bold tracking-wider md:col-start-2 md:text-6xl">
         I build <span className="text-purple">web applications.</span>
       </h3>
       <h4 className="p-4 text-justify text-lg md:col-start-2">
@@ -22,7 +16,9 @@ export default function Hero() {
         always having accessibility in mind.
       </h4>
       <div className="p-10 text-center md:col-start-2">
-        <Button color="bg-green" large text="Check out my resume" url="#" />
+        <button type="button" className="button w-48 bg-green">
+          Check out my resume
+        </button>
       </div>
     </section>
   );
