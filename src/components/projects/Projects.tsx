e8623ff699d7e20e85f9c9d1a5a7bd96f393241f
aliendro <mgbaliendro@gmail.com>
2022-01-25 16:58:00 -0300

    Fix: padding to better readability on mobile devices

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -2,7 +2,7 @@ import { animehub } from 'assets';
 import ProjectItem from './ProjectItem';
 
 const styles = {
-  section: 'grid md:grid-cols-[1fr_2fr_1fr] gap-3 text-foreground h-100 cursor-default',
+  section: 'p-4 md:p-0 grid md:grid-cols-[1fr_2fr_1fr] gap-3 text-foreground h-100 cursor-default',
   heading: 'text-3xl md:col-start-2 font-bold tracking-wide pl-4 text-cyan text-center p-10',
 };
 
@@ -26,8 +26,8 @@ export default function Projects() {
             aliquam, porro ad vero numquam, temporibus repellat. Suscipit unde quasi repudiandae qui
             possimus!
           </p>
-          <p className="text-[18px] pt-10 tracking-tight">
-            Stack: React, Typescript, Redux (Redux Toolkit), CSS Modules and PostCSS.
+          <p className="pt-10 text-[18px] tracking-tight">
+            Stack: React, Typescript, Redux (Redux Toolkit), React Router, CSS Modules and PostCSS.
           </p>
         </>
       </ProjectItem>
@@ -47,8 +47,8 @@ export default function Projects() {
             aliquam, porro ad vero numquam, temporibus repellat. Suscipit unde quasi repudiandae qui
             possimus!
           </p>
-          <p className="text-[18px] pt-10">
-            Stack: React, Typescript, Redux (Redux Toolkit), TailwindCSS, CommerceJS.
+          <p className="pt-10 text-[18px]">
+            Stack: React, Typescript, Redux (Redux Toolkit), React Router, TailwindCSS, CommerceJS.
           </p>
         </>
       </ProjectItem>
