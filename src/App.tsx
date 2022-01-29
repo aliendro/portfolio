f549687b447727d1c8152c32658f0e9c78ccbccd
aliendro <mgbaliendro@gmail.com>
2022-01-29 09:33:10 -0300

    Remove: incorrect semantic elements

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,11 @@ import Sidebar from 'components/sidebar/Sidebar';
 
 export default function App() {
   return (
-    <main className="min-h-screen bg-primary">
-      <div className="flex flex-col">
-        <Navbar />
-        <Sidebar />
-        <Content />
-        <Footer />
-      </div>
-    </main>
+    <div className="flex min-h-screen flex-col bg-primary">
+      <Navbar />
+      <Sidebar />
+      <Content />
+      <Footer />
+    </div>
   );
 }
