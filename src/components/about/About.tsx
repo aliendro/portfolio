6eebe1494542362846bbfebb3a6c575d2003fc57
aliendro <mgbaliendro@gmail.com>
2022-01-29 09:35:16 -0300

    Add: title to tech stack icons

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -22,52 +22,60 @@ export default function About() {
       </h1>
       <div className="grid grid-cols-2 place-items-center gap-5 text-center md:col-start-2 md:grid-cols-3 md:gap-10">
         <div>
-          <Javascript aria-label="javascript icon" className="icon text-yellow" />
+          <Javascript
+            title="Javascript"
+            aria-label="javascript icon"
+            className="icon text-yellow"
+          />
           <span className={text}>Javascript</span>
         </div>
         <div>
-          <Html aria-label="html icon" className="icon text-red" />
+          <Html title="HTML5" aria-label="html icon" className="icon text-red" />
           <span className={text}>HTML</span>
         </div>
         <div>
-          <Css aria-label="css icon" className="icon text-orange" />
+          <Css title="CSS3" aria-label="css icon" className="icon text-orange" />
           <span className={text}>CSS</span>
         </div>
         <div>
-          <React aria-label="react icon" className="icon text-cyan" />
+          <React title="React" aria-label="react icon" className="icon text-cyan" />
           <span className={text}>React</span>
         </div>
         <div>
-          <Redux aria-label="redux icon" className="icon text-purple" />
+          <Redux title="Redux" aria-label="redux icon" className="icon text-purple" />
           <span className={text}>Redux</span>
         </div>
         <div>
-          <Tailwind aria-label="tailwind icon" className="icon text-cyan" />
+          <Tailwind title="TailwindCSS" aria-label="tailwind icon" className="icon text-cyan" />
           <span className={text}>TailwindCSS</span>
         </div>
 
         <div>
-          <TestingLibrary aria-label="testing library icon" className="icon text-red" />
+          <TestingLibrary
+            title="Testing Library"
+            aria-label="testing library icon"
+            className="icon text-red"
+          />
           <span className={text}>Testing Library</span>
         </div>
         <div>
-          <Jest aria-label="jest icon" className="icon text-[#9A405B] " />
+          <Jest title="Jest" aria-label="jest icon" className="icon text-[#9A405B] " />
           <span className={text}>Jest</span>
         </div>
         <div>
-          <Git aria-label="git icon" className="icon text-[#E94E31]" />
+          <Git title="Git" aria-label="git icon" className="icon text-[#E94E31]" />
           <span className={text}>Git</span>
         </div>
         <div>
-          <Typescript aria-label="typescript icon" className="icon text-cyan" />
+          <Typescript title="Typescript" aria-label="typescript icon" className="icon text-cyan" />
           <span className={text}>Typescript</span>
         </div>
         <div>
-          <Nextjs aria-label="nextjs icon" className="icon text-foreground" />
+          <Nextjs title="NextJS" aria-label="nextjs icon" className="icon text-foreground" />
           <span className={text}>NextJS</span>
         </div>
         <div>
-          <Router aria-label="react-router icon" className="icon text-red" />
+          <Router title="React Router" aria-label="react-router icon" className="icon text-red" />
           <span className={text}>React Router</span>
         </div>
       </div>
