78d1dcc88c5b972b00fcfc6e9138ffa796ddc9d4
Michael Aliendro <aliendrosw@gmail.com>
2022-02-23 19:10:10 -0300

    refactor: merging about and hero sections to about component

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -16,69 +16,96 @@ import {
 export default function About() {
   const text = 'text-md font-bold tracking-wide';
   return (
-    <section className="section">
-      <h1 id="stack" className="heading text-yellow">
-        Tech stack
-      </h1>
-      <div className="grid grid-cols-2 place-items-center gap-5 text-center md:col-start-2 md:grid-cols-3 md:gap-10">
-        <div>
-          <Javascript
-            title="Javascript"
-            aria-label="javascript icon"
-            className="icon text-yellow"
-          />
-          <span className={text}>Javascript</span>
+    <>
+      <section className="section">
+        <h1 className="pl-4 font-mono text-green md:col-start-2">
+          &gt; Hi, my name is <span className="duration-50 animate-pulse">_</span>
+        </h1>
+        <h2 className="text-slate-100 pl-4 text-5xl font-bold tracking-wider md:col-start-2 md:text-6xl">
+          Michael Aliendro.
+        </h2>
+        <h3 className="text-slate-200 pl-4 text-5xl font-bold tracking-wider md:col-start-2 md:text-6xl">
+          I build <span className="text-purple">web applications.</span>
+        </h3>
+        <h4 className="p-4 text-justify text-lg md:col-start-2">
+          I&apos;m a front-end developer with more than 3 years of experience in building and web
+          applications. I am focused to deliver responsive, scalable and accessible products with
+          great user experience.
+        </h4>
+        <div className="p-10 text-center md:col-start-2">
+          <button type="button" aria-label="resume" className="button w-48 bg-green">
+            Check out my resume
+          </button>
         </div>
-        <div>
-          <Html title="HTML5" aria-label="html icon" className="icon text-red" />
-          <span className={text}>HTML</span>
-        </div>
-        <div>
-          <Css title="CSS3" aria-label="css icon" className="icon text-orange" />
-          <span className={text}>CSS</span>
-        </div>
-        <div>
-          <React title="React" aria-label="react icon" className="icon text-cyan" />
-          <span className={text}>React</span>
-        </div>
-        <div>
-          <Redux title="Redux" aria-label="redux icon" className="icon text-purple" />
-          <span className={text}>Redux</span>
-        </div>
-        <div>
-          <Tailwind title="TailwindCSS" aria-label="tailwind icon" className="icon text-cyan" />
-          <span className={text}>TailwindCSS</span>
-        </div>
+      </section>
+      <section className="section">
+        <h1 id="stack" className="heading text-yellow">
+          Tech stack
+        </h1>
+        <div className="grid grid-cols-2 place-items-center gap-5 text-center md:col-start-2 md:grid-cols-3 md:gap-10">
+          <div>
+            <Javascript
+              title="Javascript"
+              aria-label="javascript icon"
+              className="icon text-yellow"
+            />
+            <span className={text}>Javascript</span>
+          </div>
+          <div>
+            <Html title="HTML5" aria-label="html icon" className="icon text-red" />
+            <span className={text}>HTML</span>
+          </div>
+          <div>
+            <Css title="CSS3" aria-label="css icon" className="icon text-orange" />
+            <span className={text}>CSS</span>
+          </div>
+          <div>
+            <React title="React" aria-label="react icon" className="icon text-cyan" />
+            <span className={text}>React</span>
+          </div>
+          <div>
+            <Redux title="Redux" aria-label="redux icon" className="icon text-purple" />
+            <span className={text}>Redux</span>
+          </div>
+          <div>
+            <Tailwind title="TailwindCSS" aria-label="tailwind icon" className="icon text-cyan" />
+            <span className={text}>TailwindCSS</span>
+          </div>
 
-        <div>
-          <TestingLibrary
-            title="Testing Library"
-            aria-label="testing library icon"
-            className="icon text-red"
-          />
-          <span className={text}>Testing Library</span>
+          <div>
+            <TestingLibrary
+              title="Testing Library"
+              aria-label="testing library icon"
+              className="icon text-red"
+            />
+            <span className={text}>Testing Library</span>
+          </div>
+          <div>
+            <Jest title="Jest" aria-label="jest icon" className="icon text-[#9A405B] " />
+            <span className={text}>Jest</span>
+          </div>
+          <div>
+            <Git title="Git" aria-label="git icon" className="icon text-[#E94E31]" />
+            <span className={text}>Git</span>
+          </div>
+          <div>
+            <Typescript
+              title="Typescript"
+              aria-label="typescript icon"
+              className="icon text-cyan"
+            />
+            <span className={text}>Typescript</span>
+          </div>
+          <div>
+            <Nextjs title="NextJS" aria-label="nextjs icon" className="icon text-foreground" />
+            <span className={text}>NextJS</span>
+          </div>
+          <div>
+            <Node title="NodeJS" aria-label="nodejs icon" className="icon text-green" />
+            <span className={text}>NodeJS</span>
+          </div>
         </div>
-        <div>
-          <Jest title="Jest" aria-label="jest icon" className="icon text-[#9A405B] " />
-          <span className={text}>Jest</span>
-        </div>
-        <div>
-          <Git title="Git" aria-label="git icon" className="icon text-[#E94E31]" />
-          <span className={text}>Git</span>
-        </div>
-        <div>
-          <Typescript title="Typescript" aria-label="typescript icon" className="icon text-cyan" />
-          <span className={text}>Typescript</span>
-        </div>
-        <div>
-          <Nextjs title="NextJS" aria-label="nextjs icon" className="icon text-foreground" />
-          <span className={text}>NextJS</span>
-        </div>
-        <div>
-          <Node title="NodeJS" aria-label="nodejs icon" className="icon text-green" />
-          <span className={text}>NodeJS</span>
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,14 +1,12 @@
 import { lazy } from 'react';
 
 const About = lazy(() => import('components/about/About'));
-const Hero = lazy(() => import('components/hero/Hero'));
 const Projects = lazy(() => import('components/projects/Projects'));
 const GoTop = lazy(() => import('./GoTop'));
 
 export default function Content() {
   return (
     <div className="mt-32 flex w-full flex-col justify-center gap-12 md:mt-80 md:gap-40">
-      <Hero />
       <About />
       <Projects />
       <GoTop />
